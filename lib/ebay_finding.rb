b07cce6404abc4b71fe925d7efd87fe3a171183b
Rob Lambert <rob@zabada.com>
2010-06-26 18:49:30 -0500

    remove parsing of data, just raw results returned

--- lib/ebay_finding.rb
+++ lib/ebay_finding.rb
@@ -1,13 +1,12 @@
 require 'open-uri'
-require 'json'
 
-# module to ease retrieving data from the Ebay Finding API: http://developer.ebay.com/products/finding/
-module EbayFinding
+# class to ease retrieving data from the Ebay Finding API: http://developer.ebay.com/products/finding/
+class EbayFinding
   
-  # calls the "findItemsAdvanced" operation and returns the 'findItemsAdvancedResponse' JSON as a Hash.
+  # calls the "findItemsAdvanced" operation.
   # aims to make it easy to search by keyword and category as that's the most common search that I perform!
   # extra_params are used explicitly in the request and override any base parameters including app_id, affiliate keys, etc.  
-  def self.find_items( keywords, category = nil, numResults = 5, sort = :end_time_soonest, extra_params = {} )
+  def find_items( keywords, category = nil, numResults = 5, sort = :end_time_soonest, extra_params = {} )
     params = {
       "paginationInput.entriesPerPage" => numResults,
       "sortOrder" => SORT_OPTIONS[sort]||sort
@@ -15,62 +14,20 @@ module EbayFinding
     params['keywords'] = keywords if keywords
     params['categoryId'] = TOP_LEVEL_US_CATEGORIES[category]||category if TOP_LEVEL_US_CATEGORIES[category]||category
     params.merge!(extra_params)
-    fetch_as_json(build_url(:find_items, params))['findItemsAdvancedResponse']
+    fetch(build_url(:find_items, params))
   end
   
-  # calls the "getHistograms" operation for a given category and returns the 'getHistogramsResponse' JSON as a Hash
-  def self.histograms( category, extra_params = {})
+  # calls the "getHistograms" operation for a given category
+  def histograms( category, extra_params = {})
     params = { "categoryId" => TOP_LEVEL_US_CATEGORIES[category]||category }.merge!(extra_params)
-    fetch_as_json(build_url(:histograms, params))['getHistogramsResponse']
+    fetch(build_url(:histograms, params))
   end
   
-  # calls the "getKeywordsRecommendations" operation for the provided keywords argument and returns the 'getSearchKeywordsRecommendationResponse' JSON as a Hash
-  def self.keyword_recommendations(keywords, extra_params = {})
-    fetch_as_json(build_url(:keyword_recommendations, {'keywords'=>keywords}.merge!(extra_params)))['getSearchKeywordsRecommendationResponse']
+  # calls the "getKeywordsRecommendations" operation for the provided keywords argument
+  def keyword_recommendations(keywords, extra_params = {})
+    fetch(build_url(:keyword_recommendations, {'keywords'=>keywords}.merge!(extra_params)))
   end
 
-  private
-
-  def self.fetch_as_json(url)
-    JSON.parse( open(url, "UserAgent" => config_params[:user_agent]).read )
-  end
-
-  def self.build_url(operation, operation_params = {})
-    # build standard app_id and affiliate parameters
-    params = STANDARD_PARAMETERS.keys.inject({}) { |params,key| params[STANDARD_PARAMETERS[key]] = config_params[key] if config_params[key]; params }
-    # add operation parameter
-    params['OPERATION-NAME'] = OPERATIONS.fetch(operation) || operation
-    # add operation_params provided
-    params.merge!(operation_params)
-    "#{BASE_URL}#{params.keys.inject(""){|string,key| "#{string}&#{key}=#{CGI.escape(params[key].to_s)}"}}"
-  end
-  
-  # access to configuration parameters stored in ebay-finding.yml
-  def self.config_params
-    return @@config_params if @@config_params
-    params = YAML.load_file("#{RAILS_ROOT}/config/ebay_finding.yml")
-    @@config_params = params[RAILS_ENV.to_sym] || params[:production]
-  end
-
-  @@config_params = nil
-  
-  BASE_URL = "http://svcs.ebay.com/services/search/FindingService/v1?RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD"
-
-  # Item sort options. See http://developer.ebay.com/devzone/finding/CallRef/findItemsByKeywords.html#Request.sortOrder
-  SORT_OPTIONS = {
-    :best_match => "BestMatch", # 	Sorts items by Best Match, which is based on community buying activity and other relevance-based factors (default sort for most methods)
-    :bid_count_fewest => "BidCountFewest",  #Sorts items by the number of bids they have received, with items that have received the fewest bids first. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
-    :bid_count_most => "BidCountMost", # Sorts items by the number of bids they have received, with items that have received the most bids first. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
-    :country_ascending => "CountryAscending", # Sorts items available on the the given site (as specified by global ID in the HTTP header or URL parameter) by the country in which they are located. For CountryAscending, items located in the country most closely associated with the site appear first, followed by items in related countries, and then items from other countries. CountryAscending applies to the following sites only: Austria (EBAY-AT), Belgium-French (EBAY-FRBE), Belgium-Netherlands (EBAY-NLBE), Germany (EBAY-DE), Ireland (EBAY-IE), Netherlands (EBAY-NL), Poland (EBAY-PL), Spain (EBAY-ES), and Switzerland (EBAY-CH). 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
-    :country_descending => "CountryDescending", #Sorts items available on the the given site (as specified by global ID in the HTTP header or URL parameter) by the country in which they are located. For CountryDescending, items are sorted in reverse order of CountryAscending. That is, items in countries not specifically related to the site appear first, sorted in descending alphabetical order by English country name. For example, when searching the Ireland site, items located in countries like Yugoslavia or Uganda are returned first. Items located in Ireland (IE) will be returned last.
-    :current_price_highest => "CurrentPriceHighest", #	Sorts items by their current price, with the highest price first. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
-    :distance_nearest => "DistanceNearest", #	Sorts items by distance from the buyer in ascending order. The request must also include a buyerPostalCode. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
-    :end_time_soonest => "EndTimeSoonest", # Sorts items by end time, with items ending soonest listed first. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
-    :price_plus_shipping_highest => "PricePlusShippingHighest", # Sorts items by the combined cost of the item price plus the shipping cost, with highest combined price items listed first. Items are returned in the following groupings: highest total-cost items (for items where shipping was properly specified) appear first, followed by freight- shipping items, and then items for which no shipping was specified. Each group is sorted by price. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
-    :price_plus_shipping_lowest => "PricePlusShippingLowest", # Sorts items by the combined cost of the item price plus the shipping cost, with the lowest combined price items listed first. Items are returned in the following groupings: lowest total-cost items (for items where shipping was properly specified) appear first, followed by freight- shipping items, and then items for which no shipping was specified. Each group is sorted by price. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
-    :start_time_newest => "StartTimeNewest" # Sorts items by the start time, the most recently listed (newest) items appear first.
-  }
-  
   # top level category ids for U.S., pulled from Trading API May 28, 2010
   TOP_LEVEL_US_CATEGORIES = {
     :antiques=>"20081",
@@ -109,6 +66,51 @@ module EbayFinding
     :partner=>"10159"
   }
 
+  private
+  
+  def fetch(url)
+    open(url, "UserAgent" => config_params[:user_agent]).read
+  end
+
+  def build_url(operation, operation_params = {})
+    # build standard app_id and affiliate parameters
+    params = STANDARD_PARAMETERS.keys.inject({}) { |params,key| params[STANDARD_PARAMETERS[key]] = config_params[key] if config_params[key]; params }
+    # add operation parameter
+    params['OPERATION-NAME'] = OPERATIONS.fetch(operation) || operation
+    # add operation_params provided
+    params.merge!(operation_params)
+    url = "#{BASE_URL}#{params.keys.inject(""){|string,key| "#{string}&#{key}=#{CGI.escape(params[key].to_s)}"}}"
+    puts url
+    url
+  end
+  
+  # access to configuration parameters stored in ebay_finding.yml
+  def config_params
+    return @@config_params if @@config_params
+    params = YAML.load_file("#{RAILS_ROOT}/config/ebay_finding.yml")
+    @@config_params = params[RAILS_ENV.to_sym] || params[:production]
+  end
+
+  @@config_params = nil
+  
+  BASE_URL = "http://svcs.ebay.com/services/search/FindingService/v1?REST-PAYLOAD"
+
+  # Item sort options. See http://developer.ebay.com/devzone/finding/CallRef/findItemsByKeywords.html#Request.sortOrder
+  SORT_OPTIONS = {
+    :best_match => "BestMatch", # 	Sorts items by Best Match, which is based on community buying activity and other relevance-based factors (default sort for most methods)
+    :bid_count_fewest => "BidCountFewest",  #Sorts items by the number of bids they have received, with items that have received the fewest bids first. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
+    :bid_count_most => "BidCountMost", # Sorts items by the number of bids they have received, with items that have received the most bids first. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
+    :country_ascending => "CountryAscending", # Sorts items available on the the given site (as specified by global ID in the HTTP header or URL parameter) by the country in which they are located. For CountryAscending, items located in the country most closely associated with the site appear first, followed by items in related countries, and then items from other countries. CountryAscending applies to the following sites only: Austria (EBAY-AT), Belgium-French (EBAY-FRBE), Belgium-Netherlands (EBAY-NLBE), Germany (EBAY-DE), Ireland (EBAY-IE), Netherlands (EBAY-NL), Poland (EBAY-PL), Spain (EBAY-ES), and Switzerland (EBAY-CH). 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
+    :country_descending => "CountryDescending", #Sorts items available on the the given site (as specified by global ID in the HTTP header or URL parameter) by the country in which they are located. For CountryDescending, items are sorted in reverse order of CountryAscending. That is, items in countries not specifically related to the site appear first, sorted in descending alphabetical order by English country name. For example, when searching the Ireland site, items located in countries like Yugoslavia or Uganda are returned first. Items located in Ireland (IE) will be returned last.
+    :current_price_highest => "CurrentPriceHighest", #	Sorts items by their current price, with the highest price first. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
+    :distance_nearest => "DistanceNearest", #	Sorts items by distance from the buyer in ascending order. The request must also include a buyerPostalCode. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
+    :end_time_soonest => "EndTimeSoonest", # Sorts items by end time, with items ending soonest listed first. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
+    :price_plus_shipping_highest => "PricePlusShippingHighest", # Sorts items by the combined cost of the item price plus the shipping cost, with highest combined price items listed first. Items are returned in the following groupings: highest total-cost items (for items where shipping was properly specified) appear first, followed by freight- shipping items, and then items for which no shipping was specified. Each group is sorted by price. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
+    :price_plus_shipping_lowest => "PricePlusShippingLowest", # Sorts items by the combined cost of the item price plus the shipping cost, with the lowest combined price items listed first. Items are returned in the following groupings: lowest total-cost items (for items where shipping was properly specified) appear first, followed by freight- shipping items, and then items for which no shipping was specified. Each group is sorted by price. 	findItemsAdvanced, findItemsByCategory, findItemsByKeywords, findItemsByProduct, findItemsIneBayStores
+    :start_time_newest => "StartTimeNewest" # Sorts items by the start time, the most recently listed (newest) items appear first.
+  }
+
+
   OPERATIONS = {
     :keyword_recommendations => "getSearchKeywordsRecommendation", # Get recommended keywords for search
     :find_by_keywords => "findItemsByKeywords", # Search items by keywords
@@ -124,7 +126,8 @@ module EbayFinding
     :affiliate_tracking_id => "affiliate.trackingId",
     :affiliate_network_id => "affiliate.networkId",
     :affiliate_custom_id => "affiliate.customId",
-    :global_id => "GLOBAL-ID"
+    :global_id => "GLOBAL-ID",
+    :default_response_format => "RESPONSE-DATA-FORMAT"
   }
 
   # GLOBAL_IDS = {
